a975778f79c5c2ebb2e2f68e0697aeef0dc28568
NacharVS <60977438+NacharVS@users.noreply.github.com>
2023-09-04 08:14:20 +0300

    ппп

---
+++ UnitsDrafts/HP.cs
@@ -0,0 +1,28 @@
+﻿namespace UnitsDrafts
+{
+    class HP
+    {
+        static void Main(string[] args)
+        {
+            Cleric cleric = new Cleric(30, 20);
+            Archer archer = new Archer();
+
+            Unit[] units = new Unit[] { cleric, archer };
+
+            int[] healthValues = new int[units.Length];
+
+            for (int i = 0; i < units.Length; i++)
+            {
+                healthValues[i] = units[i].Health;
+            }
+
+            Array.Sort(healthValues);
+
+            Console.WriteLine("Health values in ascending order:");
+            foreach (int value in healthValues)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ UnitsDrafts/WatchingTower.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsDrafts
+{
+    internal class WatchingTower
+    {
+        private List<Archer> units = new List<Archer> { };
+        private int _damage = 0;
+        public int freeplaces;
+        public int level;
+        private int _health = 20;
+        public int _maxhealth = 20;
+        public WatchingTower(List<Archer> Units, int Level)
+        {
+            if (Level == 1 || Level == 2)
+            {
+                level = Level;
+            }
+            else
+            {
+                level = 1;
+            }
+            int unitsCount = 0;
+            foreach (Archer creatures in Units)
+            {
+                if (creatures != null)
+                {
+                    unitsCount++;
+                }
+            }
+            if (unitsCount > 1)
+            {
+                unitsCount = 1;
+            }
+            for (int i = 0; i < unitsCount; i++)
+            {
+                units.Add(Units[i]);
+                _damage += Units[i].Damage;
+            }
+            freeplaces = level - unitsCount;
+        }
+        public int Damage
+        {
+            get { return _damage; }
+            set { _damage = value; }
+        }
+        public int Health
+        {
+            get { return _health; }
+            set { _health = value; }
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Уровень башни - {level}");
+            Console.WriteLine($"урон башни - {Damage}");
+            Console.WriteLine("Персонажи в башне:");
+            foreach (Archer unit in units)
+            {
+                Console.WriteLine(unit.Name);
+            }
+            Console.WriteLine($"Свободных мест в башне - {freeplaces}");
+        }
+        public void InflictDamage(Unit unit)
+        {
+            unit.Health -= (_damage - unit.Defense);
+        }
+    }
+
+}
