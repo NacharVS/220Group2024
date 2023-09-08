cb1761e761820505d7b5c23e01736c39afdbe1fa
Suelol <kamalovramil432@gmail.com>
2023-09-08 23:20:07 +0300

    init branch

--- Kamalov220/Program.cs
+++ Kamalov220/Program.cs
@@ -11,7 +11,7 @@ namespace Kamalov220
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            Console.ReadKey();
+            Console.ReadKey(); 
         }
     }
 }
