f831400905335be8f04895902c7d5f1598a5b07a
r3dinc <r3dinc@yandex.ru>
2023-10-18 14:13:45 +0300

    list

--- UnitsDrafts/Program.cs
+++ UnitsDrafts/Program.cs
@@ -1,5 +1,23 @@
 ﻿using UnitsDrafts;
 
+List<Unit> units = new List<Unit>();
+units.Add(new Footman());
+units.Add(new Peasant());
+units.Add(new Archer()); 
+units.Add(new Peasant());
+units.Add(new Archer());
+units.Add(new Footman());
+units.Add(new Footman());
+units.Add(new Archer());
+units.Add(new Footman());
+
+if (units.Exists(x => x.Name == "Footman"))
+{
+    var founded = units.Find(x => x.Name == "Footman");
+    var index = units.FindIndex(1 , 4 , x => x.Name == "Footman");
+    founded?.ShowInfo();
+    units[1].ShowInfo();
+}
 
 
 
