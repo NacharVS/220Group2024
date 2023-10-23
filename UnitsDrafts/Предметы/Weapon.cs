1988c1d81359cc28e0cee5051375a7cdc54ba6a6
timOon849 <supersprait@gmail.com>
2023-10-23 08:50:20 +0300

    fix

---
+++ UnitsDrafts/Предметы/Weapon.cs
@@ -0,0 +1,22 @@
+﻿namespace UnitsDrafts
+{
+    internal abstract class Weapon
+    {
+        protected Weapon(int minDamage, int maxDamage, int attackSpeed)
+        {
+            MinDamage = minDamage;
+            MaxDamage = maxDamage;
+            AttackSpeed = attackSpeed;
+        }
+
+        public int MinDamage { get; set; }
+
+        public int MaxDamage { get; set; }
+
+        public int AttackSpeed { get; set; }
+
+        public abstract int Hit();
+    }
+}
+
+
