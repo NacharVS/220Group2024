2c6f4c4cd9789064c420001f1082e67a4c81a177
emodendy <mrdendy1337@mail.ru>
2023-10-23 08:59:19 +0300

    fixx

---
+++ UnitsDrafts/items/Axe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsDrafts.items
+{
+    internal class Axe : Weapon
+    {
+        public Axe(int minDamage, int maxDamage, int attackSpeed) : base(minDamage, maxDamage, attackSpeed)
+        {
+        }
+
+        public override int Hit()
+        {
+            return new Random().Next(MinDamage, MaxDamage);
+        }
+    }
+}
