d8932461599bc50b9c6fcdce6b893954d7adc1a6
EduardKolpakov <kolpakovedik737@gmail.com>
2023-10-25 13:46:45 +0300

    UnitDrafts release

---
+++ UnitsDrafts/UnitsDrafts/TownHall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsDrafts
+{
+    internal class TownHall : Unit 
+    {
+        public TownHall() : base("Townhall", 100, 100, 0, 0, 0)
+        { }
+    }
+}
