c341589bbb5e6be117803f20ccd9578286b1ef78
NailSunny <gaifullinnail7@gmail.com>
2023-10-25 13:56:09 +0300

    fix

--- UnitsDrafts/Items/Weapon.cs
+++ UnitsDrafts/Items/Weapon.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace UnitsDrafts.Items
 {
-    internal class Weapon
+    internal abstract class Weapon
     {
+        protected Weapon(int minDamage, int maxDamage, int attackSpeed)
+        {
+            MinDamage = minDamage;
+            MaxDamage = maxDamage;
+            AttackSpeed = attackSpeed;
+        }
+
+        public int MinDamage { get; set; }
+        public int MaxDamage { get; set; }
+
+        public int AttackSpeed { get; set; }
+
+        public abstract int Hit();
+
+
     }
 }
