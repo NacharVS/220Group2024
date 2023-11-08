8fd56e6b4dbdd7a738a45f0fba95be3a446b54ce
dudos1k <143988858+dudos1k@users.noreply.github.com>
2023-11-08 14:11:17 +0300

    Add files via upload
    commit

---
+++ федянцева оаип/UnitsDrafts/Program.cs
@@ -0,0 +1,27 @@
+﻿using UnitsDrafts;
+using UnitsDrafts.items;
+
+
+
+Axe axe1 = new Axe();
+Barracs barracs = new Barracs();
+barracs.UpgradeWeapon(axe1);
+axe1.ShowWeaponInfo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ федянцева оаип/UnitsDrafts/Statistics.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitsDrafts
+{
+    internal static class Statistics
+    {
+        public static int FootmansCount { get; set; } = 0;
+        public static int ArchersCount { get; set; } = 0;
+        public static int PeasantCount { get; set; } = 0;
+
+        public static void ShowStatistics()
+        {
+            Console.WriteLine($"{FootmansCount} лакеев, {ArchersCount} лучников, {PeasantCount} крестьян");
+        }
+    }
+}
