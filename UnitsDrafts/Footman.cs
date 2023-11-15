7cb75677276c0e1e83578a51741252d500f70c61
NailSunny <gaifullinnail7@gmail.com>
2023-11-15 13:45:12 +0300

    fix

--- UnitsDrafts/Footman.cs
+++ UnitsDrafts/Footman.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using UnitsDrafts.Items;
 
 namespace UnitsDrafts
 {
     internal class Footman
     {
+        public delegate void InflictDamageDelegate(Unit unit);
+        public InflictDamageDelegate infDamage;
+        private int _defence;
+        private Weapon _weapon;
+
+        public override int Health
+        {
+            get => base.Health;
+            set => base.Health = value;
+        }
+        public int Defence
+        {
+            get { return _defence; }
+            set { _defence = value; }
+        }
+
+        public Footman(string name, int maxHealth, int speed, int damage, int defence)
+            : base(name, maxHealth, speed)
+        {
+
+            _defence = defence;
+        }
+
+        public Footman() : base("Footman", 60, 10)
+        {
+            _defence = 2;
+            _weapon = new Axe(2, 7, 5);
+        }
+
+        public void InflictDamage(Unit unit)
+        {
+            infDamage(unit);
+        }
+        public override void ShowInfo()
+        {
+            {
+                Console.WriteLine($"Name:{Name} Health: {Health}/{MaxHealth} Damage: {_weapon.MaxDamage} Defence: {Defence}");
+            }
+
+        }
     }
+
 }
--- UnitsDrafts/Program.cs
+++ UnitsDrafts/Program.cs
@@ -1,21 +1,40 @@
 ﻿using UnitsDrafts;
-    internal class Program
+//    internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        List<Unit> units = new List<Unit>();
+//        units.Add(new Pudge());
+//        units.Add(new Archer());
+//        Archer archer = new Archer();
+//        WatchingTower tower1 = new WatchingTower();
+//        Pudge pudge = new Pudge();
+
+//        if (units.Exists(x => x.Name == "Pudge"))
+//        {
+//            var a = units.Find(x => x.Name == "Pudge");
+
+//        }
+//        tower1.WatchingTowerDamage(pudge);
+//        pudge.BaseInfo();
+//    }
+//}
+Footman ft1 = new Footman();
+Footman ft2 = new Footman();
+Bishop bishop = new Bishop();
+
+ft1.infDamage = Method1;
+ft1.infDamage += Method2;
+ft1.infDamage += bishop.Healing;
+ft1.InflictDamage(ft2);
+
+static void Method1(Unit unit)
 {
-    static void Main(string[] args)
-    {
-        List<Unit> units = new List<Unit>();
-        units.Add(new Pudge());
-        units.Add(new Archer());
-        Archer archer = new Archer();
-        WatchingTower tower1 = new WatchingTower();
-        Pudge pudge = new Pudge();
+    Console.WriteLine($"{unit.Name} took headshot");
+}
 
-        if (units.Exists(x => x.Name == "Pudge"))
-        {
-            var a = units.Find(x => x.Name == "Pudge");
-
-        }
-        tower1.WatchingTowerDamage(pudge);
-        pudge.BaseInfo();
-    }
+static void Method2(Unit unit)
+{
+    if (unit.Health < unit._maxHealth / 2)
+        Console.WriteLine($"{unit.Name} took armBraker");
 }
