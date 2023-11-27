533ddf02215e5c8989c1469422a2896f8cc4ba1b
xbasssf <gaffarovad@icloud.com>
2023-11-27 08:42:24 +0300

    fix

--- UnitsDrafts/UnitEventArg.cs
+++ UnitsDrafts/UnitEventArg.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UnitsDrafts
+﻿namespace UnitsDrafts
 {
     internal class UnitEventArg
     {
+        public UnitEventArg(string message, int healthDifferce)
+        {
+            Message = message;
+            HealthDifferce = healthDifferce;
+        }
+
+        public string Message { get; set; }
+        public int HealthDifferce { get; set; }
     }
 }
