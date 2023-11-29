7bede4cb92a0177f2fba14ce7f6698bab9a93a1e
21Dena12 <143988912+21Dena12@users.noreply.github.com>
2023-11-29 14:01:50 +0300

    Add files via upload

---
+++ UnitsDrafts/UnitsDrafts/Archer.cs
@@ -0,0 +1,16 @@
+﻿namespace UnitsDrafts
+{
+    internal class Archer : Unit
+    {
+        public Archer() : base("Archer", 40, 40, 15, 10, 3)
+        {
+        }
+        public void InflictDamage(Unit unit)
+        {
+            if (Alive)
+            {
+                unit.Health -= (Damage - unit.Defense);
+            }
+        }
+    }
+}
