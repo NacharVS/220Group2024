c3c5103d289ba4fe9420ffbe50237fcce551f00a
Начальник отдела : Камалов Р.Р <143988827+Suelol@users.noreply.github.com>
2023-11-29 14:02:03 +0300

    Add files via upload

---
+++ UnitsDrafts/Unit.cs
@@ -0,0 +1,82 @@
+﻿namespace UnitsDrafts
+{
+    internal class Unit
+    {
+        public delegate void HealthChangedDelegate(int health, int changedValue);
+
+        public delegate void InnerDelegate(int speed);
+        public InnerDelegate InDelegate;
+
+        private readonly string _name;
+        private int _health;
+        private int _maxHealth;
+        private int _speed;
+
+        public Unit(string name, int maxHealth, 
+            int speed)
+        {
+            _name = name;
+            _health = maxHealth;
+            _maxHealth = maxHealth;
+            _speed = speed;
+         
+        }
+
+        public string Name => _name;       
+        public int MaxHealth => _maxHealth;
+
+        public int Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+
+        public virtual int Health
+        {
+            get { return _health; }
+            set 
+            {
+                if (value < 0)
+                {
+                    _health = 0;
+                }
+                else
+                {
+                    var diff = _health - value;
+                    if (diff < 0)
+                    {
+                        _health = value;
+                        HealthIncreasedEvent?.Invoke(_health, diff);
+                    }
+                    else
+                    {
+                        _health = value;
+                        HealthDecreasedEvent?.Invoke(_health, diff);
+                    }
+                }
+            }
+        }
+
+        public void InnerDelegateVoid()
+        {
+            InDelegate(_speed);
+        }
+
+        public void Moving()
+        {
+            Console.WriteLine($"{_name} is moving with {_speed} speed");
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Name:{_name} Health: {_health}/{_maxHealth}" );
+
+        }
+
+        public event HealthChangedDelegate HealthDecreasedEvent;
+        public event HealthChangedDelegate HealthIncreasedEvent;
+
+
+    }
+}
