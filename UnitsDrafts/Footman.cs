7a54d80086fb95d465a2d0c5a58bc9cd8d84c22d
timOon849 <supersprait@gmail.com>
2023-11-30 22:52:06 +0300

    fix

--- UnitsDrafts/Footman.cs
+++ UnitsDrafts/Footman.cs
@@ -1,11 +1,14 @@
-﻿namespace UnitsDrafts
+﻿using static UnitsDrafts.Footman;
+
+namespace UnitsDrafts
 {
     internal class Footman : Unit
     {
+        public delegate void InflictDamageDelegate(Unit unit);
+        public InflictDamageDelegate infDamage;
         private double _damage;
         private double _defence;
         private double _health;
-        private double _maxHealth;
 
 
         public Footman(string name, int health, int maxHealth, int speed, int damage, int defence) : base(name, health, maxHealth, defence)
@@ -19,6 +22,10 @@
         {
             Damage = 15;
             Defence = 30;
-        }        
+        }
+        public void InflictDamage(Unit unit)
+        {
+            infDamage(unit);
+        }
     }
 }
--- UnitsDrafts/Program.cs
+++ UnitsDrafts/Program.cs
@@ -7,59 +7,71 @@ using System.Text;
 using System.Threading.Tasks;
 using game;
 
-namespace game
+Footman ft1 = new Footman(); Footman ft2 = new Footman();
+Bishop bishop = new Bishop();
+ft1.infDamage = Method1; ft1.infDamage = Method2;
+ft1.infDamage += bishop.HealSomebody; ft1.InflictDamage(ft2);
+static void Method1(Unit unit)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            List<Unit> units = new List<Unit>();
-            units.Add(new Footman());
-            units.Add(new Archer());
-            Archer archer = new Archer();
-            Footman footman = new Footman();
-            Archer ar = new Archer();
-            Footman f1 = new Footman();
-            Bishop bs = new Bishop(20);
-            WatchingTower wt = new WatchingTower();
+    Console.WriteLine($"{unit.Name} took headshot");
+}
+static void Method2(Unit unit)
+{
+    if (unit.Health < unit.MaxHealth / 2) Console.WriteLine($"{unit.Name} took armBraker");
+}
+//namespace game
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<Unit> units = new List<Unit>();
+//            units.Add(new Footman());
+//            units.Add(new Archer());
+//            Archer archer = new Archer();
+//            Footman footman = new Footman();
+//            Archer ar = new Archer();
+//            Footman f1 = new Footman();
+//            Bishop bs = new Bishop(20);
+//            WatchingTower wt = new WatchingTower();
 
-            if (units.Exists(x => x.Name == "Pudge"))
-            {
-                var a = units.Find(x => x.Name == "Pudge");
+//            if (units.Exists(x => x.Name == "Pudge"))
+//            {
+//                var a = units.Find(x => x.Name == "Pudge");
 
-            }
+//            }
 
 
             
 
 
-            Townhall.TownHallDamage(footman);
-            footman.ShowInfo();
+//            Townhall.TownHallDamage(footman);
+//            footman.ShowInfo();
 
-            WatchingTower.WatchingTowerDamage(footman);
-
-
-
-            //bs.ShowInfo();
-            //ar.ShowInfo();
-            //f1.ShowInfo();
-
-            //ar.TakeDamage(f1);
-            //f1.ShowInfo();
+//            WatchingTower.WatchingTowerDamage(footman);
 
 
-            //bs.HealSomebody(f1);
-            //f1.ShowInfo();
-            //bs.ShowInfo();
-            //bs.HealSomebody(f1);
-            //f1.ShowInfo();
-            //bs.HealSomebody(f1);
-            //f1.ShowInfo();
-            //bs.ShowInfo();
 
-        }
-    }
-}
+//            //bs.ShowInfo();
+//            //ar.ShowInfo();
+//            //f1.ShowInfo();
+
+//            //ar.TakeDamage(f1);
+//            //f1.ShowInfo();
+
+
+//            //bs.HealSomebody(f1);
+//            //f1.ShowInfo();
+//            //bs.ShowInfo();
+//            //bs.HealSomebody(f1);
+//            //f1.ShowInfo();
+//            //bs.HealSomebody(f1);
+//            //f1.ShowInfo();
+//            //bs.ShowInfo();
+
+//        }
+//    }
+//}
 
 
 
