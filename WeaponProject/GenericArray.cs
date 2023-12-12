56bd8acf603e32e688d861e646aad38437ad3b9f
emodendy <mrdendy1337@mail.ru>
2023-12-12 23:00:29 +0300

    dffgd

---
+++ WeaponProject/GenericArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponProject
+{
+    internal class GenericArray <K>
+    {
+        K[] mass = new K[10];
+        public void AddProducts(K obj)
+        {
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] != null)
+                {
+
+                }
+                else
+                {
+                    mass[i] = obj;
+                }
+            }
+
+        }
+
+        public K[] GetArray()
+        {
+            return mass;
+        }
+    }
+}
+
