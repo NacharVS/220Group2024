7b1f1324d9ef6d39c56717b3286b50c41822c1cc
xbasssf <gaffarovad@icloud.com>
2024-01-16 11:08:20 +0300

    fix

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,99 +24,96 @@ namespace WpfApp1
         {
            InitializeComponent();
 
-            foreach (UIElement el in MainRoot.Children)
-            {
-                if(el is Button)
-                {
-                    ((Button)el).Click += Button_Click;
-                }
-            }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            Button btn = (Button)sender;
+            if (lab.Content == "0")
+            {
+                lab.Content = (string)btn.Content;
+            }
+            else
+            lab.Content += (string)btn.Content;
         }
 
-        //    private void Button_Click_1(object sender, RoutedEventArgs e)
-        //    {
-        //        lab.Content = "1";
-        //    }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            lab.Content += (string)btn.Content;
+        }
 
-        //    private void Button_Click_2(object sender, RoutedEventArgs e)
-        //    {
-        //        lab.Content = "2";
-        //    }
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            lab.Content += (string)btn.Content;
+        }
 
-        //    private void Button_Click_3(object sender, RoutedEventArgs e)
-        //    {
-        //        lab.Content = "3";
-        //    }
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            lab.Content = "4";
 
-        //    private void Button_Click_4(object sender, RoutedEventArgs e)
-        //    {
-        //        lab.Content = "4";
+        }
 
-        //    }
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            lab.Content = "5";
+        }
 
-        //    private void Button_Click_5(object sender, RoutedEventArgs e)
-        //    {
-        //        lab.Content = "5";
-        //    }
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            lab.Content = "6";
+        }
 
-        //    private void Button_Click_6(object sender, RoutedEventArgs e)
-        //    {
-        //        lab.Content = "6";
-        //    }
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            lab.Content = "7";
+        }
 
-        //    private void Button_Click_7(object sender, RoutedEventArgs e)
-        //    {
-        //        lab.Content = "7";
-        //    }
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            lab.Content = "8";
+        }
 
-        //    private void Button_Click_8(object sender, RoutedEventArgs e)
-        //    {
-        //        lab.Content = "8";
-        //    }
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            lab.Content = "9";
+        }
 
-        //    private void Button_Click_9(object sender, RoutedEventArgs e)
-        //    {
-        //        lab.Content = "9";
-        //    }
+        private void Button_Click_0(object sender, RoutedEventArgs e)
+        {
+            lab.Content = "0";
+        }
 
-        //    private void Button_Click_0(object sender, RoutedEventArgs e)
-        //    {
-        //        lab.Content = "0";
-        //    }
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
 
-        //    private void Button_Click_14(object sender, RoutedEventArgs e)
-        //    {
+        }
 
-        //    }
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
 
-        //    private void Button_Click_11(object sender, RoutedEventArgs e)
-        //    {
+        }
 
-        //    }
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
 
-        //    private void Button_Click_13(object sender, RoutedEventArgs e)
-        //    {
+        }
 
-        //    }
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
 
-        //    private void Button_Click_12(object sender, RoutedEventArgs e)
-        //    {
+        }
 
-        //    }
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
 
-        //    private void Button_Click_10(object sender, RoutedEventArgs e)
-        //    {
+        }
 
-        //    }
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
 
-        //    private void Button_Click_15(object sender, RoutedEventArgs e)
-        //    {
-
-        //    }
-        //}
+        }
     }
+}
