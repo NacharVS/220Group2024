82a5bb10ca5ac9ef8fdb035f3e033c4f6331bdb2
xbasssf <gaffarovad@icloud.com>
2024-01-17 12:56:24 +0300

    fix

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        int Bufer;
+        string Action;
         public MainWindow()
         {
            InitializeComponent();
@@ -29,56 +31,107 @@ namespace WpfApp1
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Button btn = (Button)sender;
-            lab.Content += (string)btn.Content;
+            
+            if(lab.Content.ToString() == "0")
+            {
+               lab.Content = "1";
+            }
+            else 
+                lab.Content += "1";
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            Button btn = (Button)sender;
-            lab.Content += (string)btn.Content;
+            
+            if (lab.Content.ToString() == "0")
+            {
+                lab.Content = "2";
+            }
+            else
+                lab.Content += "2";
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            Button btn = (Button)sender;
-            lab.Content += (string)btn.Content;
+            
+            if (lab.Content.ToString() == "0")
+            {
+                lab.Content = "3";
+            }
+            else
+                lab.Content += "3";
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-            lab.Content = "4";
+            
+            if (lab.Content.ToString() == "0")
+            {
+                lab.Content = "4";
+            }
+            else
+                lab.Content += "4";
 
         }
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {
-            lab.Content = "5";
+            if (lab.Content.ToString() == "0")
+            {
+                lab.Content = "5";
+            }
+            else
+                lab.Content += "5";
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {
-            lab.Content = "6";
+            if (lab.Content.ToString() == "0")
+            {
+                lab.Content = "6";
+            }
+            else
+                lab.Content += "6";
         }
 
         private void Button_Click_7(object sender, RoutedEventArgs e)
         {
-            lab.Content = "7";
+            if (lab.Content.ToString() == "0")
+            {
+                lab.Content = "7";
+            }
+            else
+                lab.Content += "7";
         }
 
         private void Button_Click_8(object sender, RoutedEventArgs e)
         {
-            lab.Content = "8";
+            if (lab.Content.ToString() == "0")
+            {
+                lab.Content = "8";
+            }
+            else
+                lab.Content += "8";
         }
 
         private void Button_Click_9(object sender, RoutedEventArgs e)
         {
-            lab.Content = "9";
+            if (lab.Content.ToString() == "0")
+            {
+                lab.Content = "9";
+            }
+            else
+                lab.Content += "9";
         }
 
         private void Button_Click_0(object sender, RoutedEventArgs e)
         {
-            lab.Content = "0";
+            if (lab.Content.ToString() == "0")
+            {
+                lab.Content = "0";
+            }
+            else
+                lab.Content += "0";
         }
 
         private void Button_Click_14(object sender, RoutedEventArgs e)
