7efc7d6a2d758162bb84477b14a6d046b2f1ccfa
emodendy <mrdendy1337@mail.ru>
2024-01-22 20:04:51 +0300

    dsf

---
+++ WphRwgistration/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WphRwgistration
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone_number { get; set; }
+        public string Email { get; set; }
+        public User() { }
+    }
+}
