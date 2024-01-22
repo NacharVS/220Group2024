b63a76de6eb5370d8803960f21a7f36ac769db43
Suelol <kamalovramil432@gmail.com>
2024-01-22 22:09:13 +0300

    Commit

---
+++ YandexWeather/Class/WeatherService.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexWeather.Class
+{
+    public class WeatherService
+    {
+        private readonly HttpClient client;
+        private readonly string apiKey;
+
+        public WeatherService()
+        {
+            client = new HttpClient();
+            apiKey = "19fe6f14-aea0-4983-ad3c-f744af8c0560";
+        }
+
+        public async Task<JObject> GetWeatherAsync(string location)
+        {
+            string apiKey = "19fe6f14-aea0-4983-ad3c-f744af8c0560";
+            string[] coordinates = location.Split(' ');
+
+            if (coordinates.Length != 2)
+            {
+                Console.WriteLine("Location is not in the correct format.");
+                return null;
+            }
+
+            string apiUrl = $"https://api.weather.yandex.ru/v2/forecast?lat={coordinates[0]}&lon={coordinates[1]}&lang=ru_RU&apikey={apiKey}";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-Yandex-API-Key", apiKey);
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                JObject weatherData = JObject.Parse(responseBody);
+                return weatherData;
+            }
+        }
+    }
+}
---
+++ YandexWeather/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using Npgsql;
+using NpgsqlTypes;
+using System.Data.SqlClient;
+using YandexWeather.Class;
+
+namespace YandexWeather
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var weatherService = new WeatherService();
+            var databaseService = new DatabaseService();
+
+            while (true)
+            {
+                Console.WriteLine("Введите координаты населенного пункта:");
+                Console.WriteLine("Пример: 55.79 49.13");
+                string location = Console.ReadLine();
+                var weather = await weatherService.GetWeatherAsync(location);
+                if (weather != null)
+                {
+                    databaseService.SaveWeatherData(weather);
+                }
+                else
+                {
+                    Console.WriteLine("Данные о погоде не получены.");
+                }
+                Console.WriteLine("Ждем 10 минут перед следующим запросом.");
+                await Task.Delay(10 * 60 * 1000); // 10 минут
+            }
+        }
+    }
+}
