d405bdb2f338bfd631f0a57495cc0e80d215facb
nyponn <143989340+nyponn@users.noreply.github.com>
2024-01-22 21:37:37 +0300

    Add files via upload

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement el in POP.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            if (str == "AC")
+                PUP.Text = "";
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(PUP.Text, null).ToString();
+                PUP.Text = value;
+            }
+            else if (str == "cancel")
+            {
+                PUP.Text = PUP.Text.Remove(PUP.Text.Length - 1);
+            }
+            else
+                PUP.Text += str;
+        }    
+    }
+}
