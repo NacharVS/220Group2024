709f49404c06d3a3fcef60f87a3fb125d3a164c6
fen4yara <95682264+fen4yara@users.noreply.github.com>
2024-01-23 10:49:17 +0300

    Add files via upload

---
+++ registr/registr/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace registr
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<user> users = new List<user>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void clck(object sender, RoutedEventArgs e)
+        {
+            users.Add(new user(name.Text, surname.Text, otchestvo.Text, phone.Text, email.Text));
+            listik.Items.Clear();
+            foreach(var i in users)
+            {
+                listik.Items.Add(i.Name);
+            }
+            
+        }
+
+        private void listik_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            user user1 = users.Find(em => em.Name == listik.SelectedValue.ToString());
+            MessageBox.Show($"{user1.Name}\n{user1.Surname}\n{user1.Otchestvo}\n{user1.Phone}\n{user1.Email}");
+        }
+    }
+}
---
+++ registr/registr/user.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace registr
+{
+    internal class user
+    {
+        public user(string name, string surname, string otchestvo, string phone, string email)
+        {
+            Name = name;
+            Surname = surname;
+            Otchestvo = otchestvo;
+            Phone = phone;
+            Email = email;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Otchestvo { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
