faae9861bbd100670ab8b31cd1790ad868cdab6b
Karim Shakirov <kkarimyc779@gmail.com>
2024-01-26 22:19:52 +0300

    shakir registration

--- WpfLibrary3/App.xaml.cs
+++ WpfLibrary3/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WphRwgistration
+namespace WpfLibrary3
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
---
+++ WpfLibrary3/Window1.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfLibrary3
+{
+    public partial class Window1 : Window
+    {
+        string leftValue = "";
+        string rightValue = "";
+        string operation = "";
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (operation == "")
+            {
+                leftValue += button.Content.ToString();
+                displayBox.Text = leftValue;
+            }
+            else
+            {
+                rightValue += button.Content.ToString();
+                displayBox.Text = rightValue;
+            }
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            operation = button.Content.ToString();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            leftValue = "";
+            rightValue = "";
+            operation = "";
+            displayBox.Text = "";
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            double num1 = double.Parse(leftValue);
+            double num2 = double.Parse(rightValue);
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        displayBox.Text = "Error";
+                    }
+                    break;
+            }
+
+            displayBox.Text = result.ToString();
+            leftValue = result.ToString();
+            rightValue = "";
+            operation = "";
+        }
+    }
+}
