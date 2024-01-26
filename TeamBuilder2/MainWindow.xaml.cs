396298bebdff3dd3dd3f1e609a746a2f5ec13476
daliya0606 <Sultanova.daliya@bk.ru>
2024-01-26 22:38:39 +0300

    fix

---
+++ TeamBuilder2/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBase1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string action;
+        public string number1;
+        public bool clear;
+        public MainWindow()
+        {
+            clear = false;
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_action_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            action = (string)btn.Content;
+            number1 = vvod.Text;
+            clear = true;
+        }
+
+        private void btn_ravno_Click(object sender, RoutedEventArgs e)
+        {
+            double dNumber1, dNumber2, result;
+            result = 0;
+            dNumber1 = Convert.ToDouble(number1);
+            dNumber2 = Convert.ToDouble(vvod.Text);
+            if (action == "+")
+            {
+                result = dNumber1 + dNumber2;
+            }
+            if (action == "-")
+            {
+                result = dNumber1 - dNumber2;
+            }
+            if (action == "*")
+            {
+                result = dNumber1 * dNumber2;
+            }
+            if (action == "/")
+            {
+                if (dNumber2 == 0)
+                {
+                    vvod.Text = "Ошибка";
+                    MessageBox.Show("Ошибка");
+                }
+                else
+                {
+                    result = dNumber1 / dNumber2;
+                }
+            }
+            action = "=";
+            clear = true;
+            vvod.Text = result.ToString();
+        }
+        private void btn_C_Click(object sender, RoutedEventArgs e)
+        {
+            vvod.Text = "0";
+        }
+
+    }
+}
---
+++ TeamBuilder2/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfRegistration
+{
+    internal class User
+    {
+
+        public User(string name, string surname, string age, string email)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Email = email;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Age { get; set; }
+        public string Email { get; set; }
+
+        //public static List<User> GetUsers()
+        //{
+        //    List<User> users = new List<User>();
+        //    users.Add(new User("Bob", ""));
+        //    users.Add(new User("Jeff", 33));
+        //    users.Add(new User("John", 24));
+        //    users.Add(new User("Sam", 25));
+        //    users.Add(new User("Jim", 34));
+        //    return users;
+        //}
+
+        //public static List<string> GetUsersNames()
+        //{
+        //    List<string> list = new List<string>();
+        //    foreach (var item in GetUsers())
+        //    {
+        //        list.Add(item.Name);
+        //    }
+        //    return list;    
+        //}
+    }
+}
