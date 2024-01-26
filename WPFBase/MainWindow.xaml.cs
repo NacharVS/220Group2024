2791267efd54d45b649f5ea9de21465d6b9134de
timOon849 <supersprait@gmail.com>
2024-01-27 00:17:48 +0300

    calculatorFix

--- WPFBase/MainWindow.xaml.cs
+++ WPFBase/MainWindow.xaml.cs
@@ -19,117 +19,75 @@ namespace Calculator
         double operand1 = 0;
         double operand2 = 0;
         string operatori = "";
+        bool newOperand = true;
 
-        //private void Button_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Button button = sender as Button;
-        //    string text = button.Content.ToString();
-        //    if (textBlock.Text == "0")
-        //    {
-        //        textBlock.Text = "";
-        //    }
-        //    if (double.TryParse(text, out _))
-        //    {
-        //        textBlock.Text += text;
-        //    }
-        //    else if (text == "+" || text == "-" || text == "*" || text == "/")
-        //    {
-        //        operand1 = double.Parse(textBlock.Text);
-        //        operatori = text;
-        //        textBlock.Text = "";
-        //    }
-        //    else if (text == "=")
-        //    {
-        //        Equals_Click();
-        //    }
-        //    else if (text == "AC")
-        //    {
-        //        operand1 = 0;
-        //        operatori = "";
-        //        textBlock.Text = "0";
-        //    }
-        //}
-        //private void Operator_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Button button = (Button)sender;
-        //    operatori = button.Content.ToString();
-        //    operand1 = double.Parse(textBlock.Text);
-        //    textBlock.Text = "";
-        //}
-
-        //private void Equals_Click()
-        //{
-        //    operand2 = double.Parse(textBlock.Text);
-
-        //    switch (operatori)
-        //    {
-        //        case "+":
-        //            textBlock.Text = (operand1 + operand2).ToString();
-        //            break;
-        //        case "-":
-        //            textBlock.Text = (operand1 - operand2).ToString();
-        //            break;
-        //        case "*":
-        //            textBlock.Text = (operand1 * operand2).ToString();
-        //            break;
-        //        case "/":
-        //            textBlock.Text = (operand1 / operand2).ToString();
-        //            break;
-        //    }
-        //}
-
-        private void Clear_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text = "";
-            operand1 = 0;
-            operand2 = 0;
-            operatori = "";
-        }
-
-        private List<string> operationStack = new List<string>();
-
-        private void PerformOperation(string operation)
-        {
-            if (operand1 != 0)
+            Button button = sender as Button;
+            string text = button.Content.ToString();
+            if (text == "0" || text == "1" || text == "2" || text == "3" || text == "4" || text == "5" || text == "6" || text == "7" || text == "8" || text == "9")
             {
-                Equals_Click();
+                if (newOperand)
+                {
+                    textBlock.Text = "";
+                    newOperand = false;
+                }
+                textBlock.Text += text;
             }
-
-            operationStack.Add(operation);
-            operatori = operation;
-            operand1 = double.Parse(textBlock.Text);
-            textBlock.Text = "";
+            else if (text == "+" || text == "-" || text == "*" || text == "/")
+            {
+                if (operatori == "" && double.TryParse(textBlock.Text, out _))
+                {
+                    operand1 = double.Parse(textBlock.Text);
+                    operatori = text;
+                    textBlock.Text = "";
+                    newOperand = true;
+                }
+                else if (operatori != "" && double.TryParse(textBlock.Text, out _))
+                {
+                    operand2 = double.Parse(textBlock.Text);
+                    Equals_Click();
+                    operatori = text;
+                    textBlock.Text = "";
+                }
+            }
+            else if (text == "=")
+            {
+                if (operatori != "" && double.TryParse(textBlock.Text, out _))
+                {
+                    operand2 = double.Parse(textBlock.Text);
+                    Equals_Click();
+                    operatori = "";
+                    newOperand = true;
+                }
+            }
+            else if (text == "AC")
+            {
+                operand1 = 0;
+                operatori = "";
+                textBlock.Text = "0";
+                newOperand = true;
+            }
         }
 
         private void Equals_Click()
         {
-            // Perform the operations in the correct order (PEMDAS)
-            for (int i = operationStack.Count - 1; i >= 0; i--)
+            switch (operatori)
             {
-                string operation = operationStack[i];
-                switch (operation)
-                {
-                    case "*":
-                        operand1 *= operand2;
-                        break;
-                    case "/":
-                        if (operand2 != 0)
-                        {
-                            operand1 /= operand2;
-                        }
-                        break;
-                    case "+":
-                        operand1 += operand2;
-                        break;
-                    case "-":
-                        operand1 -= operand2;
-                        break;
-                }
+                case "+":
+                    textBlock.Text = (operand1 + operand2).ToString();
+                    break;
+                case "-":
+                    textBlock.Text = (operand1 - operand2).ToString();
+                    break;
+                case "*":
+                    textBlock.Text = (operand1 * operand2).ToString();
+                    break;
+                case "/":
+                    textBlock.Text = (operand1 / operand2).ToString();
+                    break;
             }
-
-            textBlock.Text = operand1.ToString();
-            operand2 = 0;
-            operationStack.Clear();
+            operand1 = double.Parse(textBlock.Text);
         }
     }
 }
