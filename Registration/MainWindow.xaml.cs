4982fed7834d6d5dcd8e599c4d128cbfb97239f3
daliya0606 <Sultanova.daliya@bk.ru>
2024-01-27 03:59:44 +0300

    fix

---
+++ Registration/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Registration;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Registration
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<User> users;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            users = new List<User>();
+            //var globalNames = Name.Text;
+            //var globalSurnames = Surname.Text;
+            //var globalEmail = Email.Text;
+        }
+
+        private void regestratoin_Click(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User(Name.Text, Surname.Text, Age.Text, Email.Text));
+            ListBoxRefresh();
+            Name.Clear();
+            Name.Text = "Имя";
+            Surname.Clear();
+            Surname.Text = "Фамилия";
+            Age.Clear();
+            Age.Text = "Возраст";
+            Email.Clear();
+            Email.Text = "Почта";
+        }
+        private void ListBoxRefresh()
+        {
+            listbox.Items.Clear();
+            foreach (var item in users)
+            {
+                listbox.Items.Add(item.Name);
+            }
+        }
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox.SelectedItem != null)
+            {
+                string selectedName = listBox.SelectedItem.ToString();
+                User selectedUser = users.FirstOrDefault(u => u.Name == selectedName);
+            }
+            if (selectedUser != null)
+            {
+                Label.Content = ($"Имя: {selectedUser.Name},\nФамилия: {selectedUser.Surname}, \nВозраст: {selectedUser.Age}, \nEmail: {selectedUser.Email}");
+                
+            }   
+        }
+        private void listbox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+    }
+}
+
+
+
+
+
+
---
+++ Registration/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registration
+{
+    internal class User
+    {
+        public User(string name, string surname, string age, string email)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Email = email;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Age { get; set; }
+        public string Email { get; set; }
+    }
+   
+}
