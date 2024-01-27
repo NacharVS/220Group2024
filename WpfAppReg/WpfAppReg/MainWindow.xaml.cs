2be5ab423d5a221b078ff5c66d783b200dacc6e4
dudos1k <dduuddooss59@gmail.com>
2024-01-27 10:43:18 +0300

    фикс

---
+++ WpfAppReg/WpfAppReg/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace WpfAppReg
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public class Employee
+        {
+            public string Mail {  get; set; }
+            public string Name { get; set; }
+            public string LastName { get; set; }
+            public string Age {  get; set; }
+
+            public Employee(string _mail, string _name, string _lastname, string _age)
+            {
+                Mail = _mail;
+                Name = _name;
+                LastName = _lastname;
+                Age = _age;
+            }
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Employee> employees = new List<Employee> { };
+        List<Employee> employees_buffer = new List<Employee> { };
+
+        private void btnreg_Click(object sender, RoutedEventArgs e)
+        {
+            employees.Add(new Employee(Mailtext.Text, Nametext.Text, LastNametext.Text, Agetext.Text));
+            employees_buffer.Add(new Employee(Mailtext.Text, Nametext.Text, LastNametext.Text, Agetext.Text));
+            Mailtext.Clear();
+            Nametext.Clear();
+            LastNametext.Clear();
+            Agetext.Clear();
+            foreach (Employee employee in employees_buffer)
+            {
+                List1.Items.Add(employee.Name);
+            }
+            employees_buffer.Clear();
+        }
+        private void List1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string sname = List1.SelectedValue.ToString();
+            Employee emp = employees.Find(es => es.Name == sname);
+            Mailres.Content = "Mail: " + emp.Mail;
+            Nameres.Content = "Name: " + emp.Name;
+            Lastnameres.Content = "Lastname: " + emp.LastName;
+            Ageres.Content = "Age: " + emp.Age;
+        }
+
+        private void List1_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string sname = List1.SelectedValue.ToString();
+            Employee emp = employees.Find(es => es.Name == sname);
+            Mailres.Content = "Mail: " + emp.Mail;
+            Nameres.Content = "Name: " + emp.Name;
+            Lastnameres.Content = "Lastname: " + emp.LastName;
+            Ageres.Content = "Age: " + emp.Age;
+        }
+
+        private void Agetext_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Nametext_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfCalc/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCalc
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public string Sign { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var num = sender as Button;
+            string interimresult = Convert.ToString(Number2);
+
+            if (Number1 == 0)
+            {
+                if (Convert.ToInt64(Label1.Content) * Convert.ToInt64(Label1.Content) == 0)
+                {
+                    Label1.Content = num.Content.ToString();
+                }
+                else
+                {
+                    Label1.Content += num.Content.ToString();
+                }
+            }
+            else
+            {
+                Label1.Content = null;
+                if (interimresult == "0")
+                {
+                    interimresult = Convert.ToString(num.Content);
+                    Number2 = Convert.ToInt64(interimresult);
+                    Label1.Content = interimresult;
+                }
+                else
+                {
+                interimresult += Convert.ToString(num.Content);
+                Number2 = Convert.ToInt64(interimresult);
+                Label1.Content = interimresult;
+                }
+            }
+        }
+
+        private void ButtonZn_Click(object sender, RoutedEventArgs e)
+        {
+            var zn = sender as Button;
+
+            if (Number1 == 0)
+             {
+                Number1 = Convert.ToInt64(Label1.Content);
+                Label1.Content = zn.Content.ToString();
+
+                switch (Label1.Content)
+                {
+                    case "+":
+                        Sign = "+";
+                        break;
+
+                    case "-":
+                        Sign = "-";
+                        break;
+
+                    case "*":
+                        Sign = "*";
+                        break;
+
+                    case "/":
+                        Sign = "/";
+                        break;
+                }
+                Label1.Content = Number1;
+            }
+            else if (Number1 != 0 & Number2 == 0)
+            {
+                
+                Label1.Content = zn.Content.ToString();
+
+                switch (Label1.Content)
+                {
+                    case "+":
+                        Sign = "+";
+                        break;
+
+                    case "-":
+                        Sign = "-";
+                        break;
+
+                    case "*":
+                        Sign = "*";
+                        break;
+
+                    case "/":
+                        Sign = "/";
+                        break;
+                }
+
+                if (Sign == "+") { Number1 = Number1 + Number2; }
+
+                else if (Sign == "-") { Number1 = Number1 - Number2; }
+
+                else if (Sign == "*") { Number1 = Number1 * Number2; }
+
+                else { Number1 = Number1 / Number2; }
+
+                Label1.Content = Number1;
+            }
+            else
+            {
+                if (Sign == "+") { Number1 = Number1 + Number2; }
+
+                else if (Sign == "-") { Number1 = Number1 - Number2; }
+
+                else if (Sign == "*") { Number1 = Number1 * Number2; }
+
+                else { Number1 = Number1 / Number2; }
+
+                Label1.Content = zn.Content.ToString();
+
+                switch (Label1.Content)
+                {
+                    case "+":
+                        Sign = "+";
+                        break;
+
+                    case "-":
+                        Sign = "-";
+                        break;
+
+                    case "*":
+                        Sign = "*";
+                        break;
+
+                    case "/":
+                        Sign = "/";
+                        break;
+                }
+                Label1.Content = Number1;
+                Number2 = 0;
+            }
+        }
+
+        private void ButtonRvn_Click(object sender, RoutedEventArgs e)
+        {
+            Number2 = Convert.ToInt64(Label1.Content);
+
+            if (Sign == "+") { Label1.Content = Number1 + Number2; }
+
+            else if (Sign == "-") { Label1.Content = Number1 - Number2; }
+
+            else if (Sign == "*") { Label1.Content = Number1 * Number2; }
+
+            else { Label1.Content = Number1 / Number2; }
+        }
+
+        private void ButtonZero_Click(object sender, RoutedEventArgs e)
+        {
+            Label1.Content = 0;
+            Number1 = 0;
+            Number2 = 0;
+            Sign = null;
+        }
+
+    }
+}
