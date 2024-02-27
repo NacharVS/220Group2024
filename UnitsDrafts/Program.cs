89061a7ea8a17a6418b2102fc8fb035a8e3e29bd
emodendy <mrdendy1337@mail.ru>
2024-02-27 10:53:21 +0300

    dgfdfg

--- UnitsDrafts/Program.cs
+++ UnitsDrafts/Program.cs
@@ -1,6 +1,7 @@
 ﻿int[,] arr1 = new int[5, 5];
 int[,] arr2 = new int[5, 5];
 int[,] result = new int[5, 5];
+int[] res2 = new int[5];
 Task task1 = new Task(() =>
 {
     for (int i = 0; i < 5; i++)
@@ -31,5 +32,14 @@ for (int i = 0; i < 5; i++)
         result[i, j] = arr1[i, j] + arr2[i, j];
         Console.Write($"{result[i, j]} ");
     }
+}
+Console.WriteLine();
+for (int i = 0;i < 5; i++) 
+{ 
+for (int j = 0;j < 5; j++)
+    {
+        res2[i] += result[j, i];
+    }
+    Console.Write($"{res2[i]} ");
 }
 
